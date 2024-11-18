759e54969f9a18ee67e24d775d9a884fe3b261e2
keenan maupin <maupin.keenan97@gmail.com>
2024-11-18 11:31:29 -0800

    corrected server imports  that I missed

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,10 @@
 import express, { Request, Response } from 'express';
 import path from 'node:path';
 import db from './config/connection.js'
-import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/server-express
+import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './schemas/index.js';
 import { authenticateToken } from './utils/auth.js';
-import { generateResumePDF } from './/PDF_Download/pdfServices.js';
-import { Resume } from './models/User.js'; // Import your Resume model
 const server = new ApolloServer({
   typeDefs,
   resolvers
@@ -35,25 +33,7 @@ const startApolloServer = async () => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     })
   };
-
-  app.get('/download-resume/:id', async function (req: Request, res: Response) {
-      try {
-        // Fetch the resume by ID from the database
-        const resume = await Resume.findById(req.params.id);
-
-        if (!resume) {
-          return res.status(404).send('Resume not found');
-        }
-
-        // Generate and send the PDF response
-        generateResumePDF(resume);
-      } catch (error) {
-        console.error('Error generating PDF:', error);
-        res.status(500).send('Internal Server Error');
-      }return res.status(200).send('Resume successfully created.');
-    });
-
-  app.listen(PORT, () => {
+    app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
   });
