67e83e2bb34ce8d7e6ebf3a5e5490228c0a6e688
JSD <js.dohil@gmail.com>
2024-11-18 18:10:12 -0800

    chnages

---
+++ client/src/interfaces/ProfileData.tsx
@@ -0,0 +1,6 @@
+export default interface ProfileData {
+    name: string | null;
+    email: string | null;
+    address: string|null;
+    phone:string|null;
+  }
--- client/src/interfaces/index.tsx
+++ client/src/interfaces/index.tsx
@@ -1,5 +1,6 @@
 import  EducationData  from "./EducationData";
 import  ProjectData  from "./ProjectData";
 import ResumeData  from "./ResumeData";
+import ProfileData from "./ProfileData";
 import  User  from "./User";
-export type {EducationData,ProjectData,User,ResumeData};
+export type {EducationData,ProjectData,User,ResumeData,ProfileData};
