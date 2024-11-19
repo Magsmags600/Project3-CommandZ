6552012c993ff90dfbb971c770fadbd5ace56085
Balsher Sran <balshersran@Balshers-MacBook-Pro.local>
2024-11-18 17:37:11 -0800

    created an experience data form

--- client/src/interfaces/index.tsx
+++ client/src/interfaces/index.tsx
@@ -1,5 +1,6 @@
-import  EducationData  from "./EducationData";
-import  ProjectData  from "./ProjectData";
-import ResumeData  from "./ResumeData";
-import  User  from "./User";
-export type {EducationData,ProjectData,User,ResumeData};
+import EducationData from "./EducationData";
+import ProjectData from "./ProjectData";
+import ResumeData from "./ResumeData";
+import User from "./User";
+import ExperienceData from "./ExperienceData";
+export type { EducationData, ProjectData, User, ResumeData, ExperienceData };
