47971722fb3ee9bc25db5d76a9ef17b88ba8764b
Balsher Sran <balshersran@Balshers-MacBook-Pro.local>
2024-11-18 18:49:26 -0800

    added skillsdata to the homepage, fixed the component, added the card info required aswell

--- client/src/interfaces/ResumeData.tsx
+++ client/src/interfaces/ResumeData.tsx
@@ -1,15 +1,15 @@
-
-import  EducationData  from "./EducationData";
-import  ProjectData  from "./ProjectData";
+import EducationData from "./EducationData";
+import ProjectData from "./ProjectData";
+import SkillsData from "./SkillsData";
 export default interface ResumeData {
   _id: string;
   name: string;
   email: string;
-  address:string;
-  phone:string;
+  address: string;
+  phone: string;
   education: EducationData[];
   experiences: string[];
-  projects:  ProjectData[];
-  skills: string[];
+  projects: ProjectData[];
+  skills: SkillsData[];
   contacts: string[];
 }
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,23 +3,28 @@ import { useState } from "react";
 import ProfileForm from "../components/ProfileForm";
 import EducationForm from "../components/EducationComponent";
 import ProjectForm from "../components/ProjectComponent";
-import { ResumeData ,EducationData,ProjectData} from "../interfaces";
+import SkillsForm from "../components/SkillsComponent.tsx";
+import {
+  ResumeData,
+  EducationData,
+  ProjectData,
+  SkillsData,
+} from "../interfaces";
 
 const Home = () => {
   // const { loading, data } = useQuery(QUERY_PROFILES);
   // const profiles = data?.profiles || [];
-  const [resumeData,setResumeData] = useState({}as ResumeData);
+  const [resumeData, setResumeData] = useState({} as ResumeData);
   const [educationData, setEducationData] = useState<EducationData[]>([]);
-// education functions
+  // education functions
   const handleEducationChange = (index: number, field: string, value: any) => {
     console.log(typeof value);
     const updatedEducation = [...educationData];
-    updatedEducation[index] = { ...updatedEducation[index], [field]:value };
+    updatedEducation[index] = { ...updatedEducation[index], [field]: value };
     setEducationData(updatedEducation);
-    const updatedresumeData = { ...resumeData, education: updatedEducation }
+    const updatedresumeData = { ...resumeData, education: updatedEducation };
     console.log(updatedresumeData);
     setResumeData(updatedresumeData);
- 
   };
 
   const handleAddEducation = () => {
@@ -30,11 +35,10 @@ const Home = () => {
         institution: "",
         degree: "",
         fieldOfStudy: "",
-        startDate: '',
-        endDate: '',
+        startDate: "",
+        endDate: "",
       },
     ]);
-
   };
   // project functions
   const [projectData, setProjectData] = useState<ProjectData[]>([]);
@@ -43,10 +47,9 @@ const Home = () => {
     const updatedProjects = [...projectData];
     updatedProjects[index] = { ...updatedProjects[index], [field]: value };
     setProjectData(updatedProjects);
-    const updatedresumeData = { ...resumeData, projects: updatedProjects }
+    const updatedresumeData = { ...resumeData, projects: updatedProjects };
     console.log(updatedresumeData);
     setResumeData(updatedresumeData);
-    
   };
 
   const handleAddProject = () => {
@@ -56,69 +59,104 @@ const Home = () => {
         projectsId: Date.now().toString(),
         title: "",
         description: "",
-        startDate: '',
-        endDate: '',
+        startDate: "",
+        endDate: "",
       },
     ]);
   };
+
+  const [skillsData, setSkillsData] = useState<SkillsData[]>([]);
+
+  const handleSkillsChange = (index: number, field: string, value: any) => {
+    const updatedSkills = [...skillsData];
+    updatedSkills[index] = { ...updatedSkills[index], [field]: value };
+    setSkillsData(updatedSkills);
+    const updatedresumeData = { ...resumeData, skills: updatedSkills };
+    console.log(updatedresumeData);
+    setResumeData(updatedresumeData);
+  };
+
+  const handleAddSkill = () => {
+    setSkillsData([
+      ...skillsData,
+      {
+        skillsId: Date.now().toString(),
+        skills: "",
+      },
+    ]);
+  };
+
   const [count, setCount] = useState<number>(0);
-  const cards = ['profile', 'education', 'skills', 'project'];
+  const cards = ["profile", "education", "skills", "project"];
 
   const renderPage = () => {
     switch (cards[count]) {
       case "profile":
         return <ProfileForm />;
       case "education":
-        return <EducationForm handleAddEducation={handleAddEducation} handleEducationChange={handleEducationChange} educationData={educationData} />;
+        return (
+          <EducationForm
+            handleAddEducation={handleAddEducation}
+            handleEducationChange={handleEducationChange}
+            educationData={educationData}
+          />
+        );
       case "skills":
-        return '<skills />';
+        return (
+          <SkillsForm
+            handleAddSkill={handleAddSkill}
+            handleSkillsChange={handleSkillsChange}
+            skillsData={skillsData}
+          />
+        );
       case "project":
-        return <ProjectForm handleAddProject={handleAddProject} handleProjectChange={handleProjectChange} projectData={projectData}/>;
+        return (
+          <ProjectForm
+            handleAddProject={handleAddProject}
+            handleProjectChange={handleProjectChange}
+            projectData={projectData}
+          />
+        );
       default:
         return <ProfileForm />;
     }
-  }
+  };
 
   // Function to increment the count fro next step
   const increment = () => {
     if (count < cards.length - 1) {
-
-      setCount(prevCount => prevCount + 1);
+      setCount((prevCount) => prevCount + 1);
     }
   };
 
   // Function to decrement the count fro previous step
   const decrement = () => {
-
     if (count > 0) {
-      setCount(prevCount => prevCount - 1);
+      setCount((prevCount) => prevCount - 1);
     }
-
   };
-
 
   return (
     <main>
-
       <div className="d-flex justify-content-center">
-        <div className="col-12 col-md-10 my-3">
+        <div className="col-12 col-md-10 my-3">{renderPage()}</div>
 
-          {renderPage()}
-
-          
-
+        <div
+          className="col-6 col-md-10 my-3 float-left "
+          style={{ float: "left" }}
+        >
+          <button className="btn btn-info" onClick={decrement}>
+            PREVIOUS
+          </button>
         </div>
-
-        <div className="col-6 col-md-10 my-3 float-left " style={{float:"left"}}>
-            <button className="btn btn-info" onClick={decrement} >
-              PREVIOUS
-            </button>
-         
-          </div>
-        <div className="col-6 col-md-10 my-3 float-right" style={{float:"right"}} >
-         
-            <button className="btn btn-success" onClick={increment}>NEXT</button>
-          </div>
+        <div
+          className="col-6 col-md-10 my-3 float-right"
+          style={{ float: "right" }}
+        >
+          <button className="btn btn-success" onClick={increment}>
+            NEXT
+          </button>
+        </div>
       </div>
     </main>
   );
